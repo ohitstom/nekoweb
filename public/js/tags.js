41f7cda8592a4dfa253840115c6cdc4bbda9ab33
OhItsTom <22730962+ohitstom@users.noreply.github.com>
2024-07-18 18:37:39 +0100

    Refactor HTML minification and JavaScript uglification in build process

--- public/js/tags.js
+++
@@ -1,6 +0,0 @@
-console.log("parsing markdown");
-const markdownTags = document.querySelectorAll("markdown");
-markdownTags.forEach(tag => {
-	tag.innerHTML = marked.parse(tag.innerHTML.trim().replace(/\\n/g, "\n").replace(/\t/g, ""));
-	tag.setAttribute("loaded", "");
-});
